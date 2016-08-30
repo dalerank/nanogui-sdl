40115feb224cd88c6fa8924b9248d3c6e0a95830
s.kushnirenko <s.kushnirenko@gaijin.ru>
2016-08-30 15:28:53 +0300

    toolbutton moved to picogui

--- include/picogui.h
+++ include/picogui.h
@@ -1490,6 +1490,18 @@ protected:
     VectorXf mValues;
 };
 
+class ToolButton : public Button
+{
+public:
+    ToolButton(Widget *parent, int icon,
+           const std::string &caption = "")
+        : Button(parent, caption, icon)
+    {
+        setFlags(Flags::RadioButton | Flags::ToggleButton);
+        setFixedSize(Vector2i(25, 25));
+    }
+};
+
 class NANOGUI_EXPORT VScrollPanel : public Widget
 {
 public:
